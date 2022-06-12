e2d03f45f448099b2fb51b89900b308ba311c9a0
Lady Trell <antrellkent@gmail.com>
2022-06-12 11:05:19 -0400

    user model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,46 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
 
 const UserSchema = new Schema(
   {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
   }
 );
 
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.reduce(
+    (total, friends) => total + friends.replies.length + 1, 0);
+});
 
 const User = model('User', UserSchema);
 
